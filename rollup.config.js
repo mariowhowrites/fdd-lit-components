0e2a2220dbceab4dc30e69dfb26d3f25de42bc67
Mario Vega <mariovegadev@gmail.com>
2025-06-06 22:14:26 -0700

    readd umd

--- rollup.config.js
+++ rollup.config.js
@@ -4,9 +4,10 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-import resolve from '@rollup/plugin-node-resolve';
-import terser from '@rollup/plugin-terser';
 import summary from 'rollup-plugin-summary';
+import terser from '@rollup/plugin-terser';
+import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
 
 export default {
   input: 'psychopomp-dance.js',
@@ -23,13 +24,23 @@ export default {
       sourcemap: true
     }
   ],
+  onwarn(warning) {
+    if (warning.code !== 'THIS_IS_UNDEFINED') {
+      console.error(`(!) ${warning.message}`);
+    }
+  },
   plugins: [
+    replace({preventAssignment: false, 'Reflect.decorate': 'undefined'}),
     resolve(),
+    /**
+     * This minification setup serves the static site generation.
+     * For bundling and minification, check the README.md file.
+     */
     terser({
       ecma: 2020,
       module: true,
       warnings: true,
     }),
-    summary()
-  ]
+    summary(),
+  ],
 };
