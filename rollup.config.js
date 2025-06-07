56253d6f1165239f1c2410ada5258d1d5d471de2
Mario Vega <mariovegadev@gmail.com>
2025-06-06 18:46:41 -0700

    try to get it working

--- rollup.config.js
+++ rollup.config.js
@@ -4,39 +4,32 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-import summary from 'rollup-plugin-summary';
-import terser from '@rollup/plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
-import replace from '@rollup/plugin-replace';
+import terser from '@rollup/plugin-terser';
+import summary from 'rollup-plugin-summary';
 
 export default {
   input: 'psychopomp-dance.js',
-  output: {
-    file: 'psychopomp-dance.bundled.js',
-    format: 'esm',
-  },
-  onwarn(warning) {
-    if (warning.code !== 'THIS_IS_UNDEFINED') {
-      console.error(`(!) ${warning.message}`);
+  output: [
+    {
+      file: 'dist/psychopomp-dance.js',
+      format: 'es',
+      sourcemap: true
+    },
+    {
+      file: 'dist/psychopomp-dance.umd.js',
+      format: 'umd',
+      name: 'PsychopompDance',
+      sourcemap: true
     }
-  },
+  ],
   plugins: [
-    replace({preventAssignment: false, 'Reflect.decorate': 'undefined'}),
     resolve(),
-    /**
-     * This minification setup serves the static site generation.
-     * For bundling and minification, check the README.md file.
-     */
     terser({
-      ecma: 2021,
+      ecma: 2020,
       module: true,
       warnings: true,
-      mangle: {
-        properties: {
-          regex: /^__/,
-        },
-      },
     }),
-    summary(),
-  ],
+    summary()
+  ]
 };
